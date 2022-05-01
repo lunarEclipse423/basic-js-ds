881a630913dbbf550cf9332883c058d1ccf8f605
Maria White <redstone.mari@gmail.com>
2022-05-02 00:27:41 +0300

    feat: solve remove-from-list task

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,17 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  function getListWithout(list) {
+    if (list.next !== null && typeof(list.next) === 'object') {
+      list.next = getListWithout(list.next);
+    }
+    if (list.value === k) {
+      return list.next;
+    }
+    return list;
+  }
+  return getListWithout(l);
 }
 
 module.exports = {
