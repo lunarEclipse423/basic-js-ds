d2a9fd74fedee47d3c539082cba10122cf72941f
Maria White <redstone.mari@gmail.com>
2022-05-01 23:39:26 +0300

    feat: solve stack task

--- src/stack.js
+++ src/stack.js
@@ -12,20 +12,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
+
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.stack = new Array(0);
+  }
+
+  push(element) {
+    this.stack.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack.pop();
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stack.length ? this.stack[this.stack.length - 1] : 0;
   }
 }
 
